6fa565bcd9efaaae9ce94c6a999866c05fc58bae
pkkj <gz.pkkj@gmail.com>
2015-01-02 18:54:53 -0800

    New example for getFileAsync

---
+++ GetFileAsync/Handler.ashx
@@ -0,0 +1,59 @@
+﻿<%@ WebHandler Language="C#" Class="Handler" %>
+
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Web;
+using System.Web.Script.Serialization;
+
+/*
+ * This handler will accept the document data from the client, 
+ * extract the main data from the docx (which is a zip archive) and return it to the client.
+ */
+public class Handler : IHttpHandler
+{
+    public void ProcessRequest(HttpContext context)
+    {
+        if (context.Request.InputStream.Length > 0)
+        {
+            string webRequestData = "";
+            using (StreamReader sr = new StreamReader(context.Request.InputStream))
+            {
+                webRequestData = sr.ReadToEnd();
+            }
+            byte[] base64String = Convert.FromBase64String(webRequestData);
+            Stream docxStream = new MemoryStream(base64String);
+            try
+            {
+                ZipArchive archive = new ZipArchive(docxStream);
+
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    // The text content of a docx is stored in word/document.xml. So we seek the data from there.
+                    // You may do further operation with OpenXmlApi.
+                    if (entry.FullName == "word/document.xml")
+                    {
+                        Stream documentXmlData = entry.Open();
+                        StreamReader reader = new StreamReader(documentXmlData);
+                        string text = reader.ReadToEnd();
+                        context.Response.Write(text);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                context.Response.Write(ex.Message);
+            }
+        }
+        context.Response.ContentType = "text/plain";
+    }
+
+    public bool IsReusable
+    {
+        get
+        {
+            return false;
+        }
+    }
+}
---
+++ GetFileAsync/app.js
@@ -0,0 +1,68 @@
+﻿(function () {
+    "use strict";
+
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            $('#getFileAsync').click(getFileAsyncInternal);
+        });
+    };
+
+    function encodeBase64(docData) {
+        var s = "";
+        for (var i = 0; i < docData.length; i++)
+            s += String.fromCharCode(docData[i]);
+        return window.btoa(s);
+    }
+
+    // Call getFileAsnyc() to start the retrieving file process.
+    function getFileAsyncInternal() {
+        Office.context.document.getFileAsync("compressed", { sliceSize: 10240 }, function (asyncResult) {
+            if (asyncResult.status == Office.AsyncResultStatus.Failed) {
+                document.getElementById("log").textContent = JSON.stringify(asyncResult);
+            }
+            else {
+                getAllSlices(asyncResult.value);
+            }
+        });
+    }
+
+    // Get all the slices of file from the host after "getFileAsync" is done.
+    function getAllSlices(file) {
+        var sliceCount = file.sliceCount;
+        var sliceIndex = 0;
+        var docdata = [];
+        var getSlice = function () {
+            file.getSliceAsync(sliceIndex, function (asyncResult) {
+                if (asyncResult.status == "succeeded") {
+                    docdata = docdata.concat(asyncResult.value.data);
+                    sliceIndex++;
+                    if (sliceIndex == sliceCount) {
+                        file.closeAsync();
+                        onGetAllSlicesSucceeded(docdata);
+                    }
+                    else {
+                        getSlice();
+                    }
+                }
+                else {
+                    file.closeAsync();
+                    document.getElementById("log").textContent = JSON.stringify(asyncResult);
+                }
+            });
+        };
+        getSlice();
+    }
+
+    // Upload the docx file to server after obtaining all the bits from host.
+    function onGetAllSlicesSucceeded(docxData) {
+        $.ajax({
+            type: "POST",
+            url: "Handler.ashx",
+            data: encodeBase64(docxData),
+            contentType: "application/json; charset=utf-8",
+        }).done(function (data) {
+            document.getElementById("documentXmlContent").textContent = data;
+        }).fail(function (jqXHR, textStatus) {
+        });
+    }
+})();
